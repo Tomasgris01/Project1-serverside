c4e004000434a01ccb1408ce376b4756cd36573e
Tomasgris01 <tomasgris14@gmail.com>
2022-04-22 21:24:37 +0100

    update done

--- routes/book.js
+++ routes/book.js
@@ -1,20 +1,34 @@
 const express = require('express');
-const { readBook  } = require('../models/book');
+const { readBook, createBook } = require('../models/book');
 const router = express.Router();
 
 
-router.get('/', async (req, res) =>
-{
-    const book = await readBook();
 
-    res.render('listing', { personlist: book })
-    
+router.post('/addnew', async (req, res) => {
+
+    // note we leave error handling for now and assume our data is created.
+    // note: this is not safe code. Any inputs from a user should be validated and sanitised before
+    // being saved to the database.
+
+    await createBook(req.body);
+
+
+    res.redirect(303, '/book')
+
+
+})
+
+router.get('/addnew', async (req, res) => {
+
+    res.render('personform')
+
+
 })
 
 router.get('/:name', async (req, res) => {
     var name = req.params.name;
 
-    const person = await readBook({'name': name})
+    const person = await readBook({ 'name': name })
 
     if (!person) {
         console.log('404 because book doesn\'t exist');
@@ -23,6 +37,26 @@ router.get('/:name', async (req, res) => {
     else {
         res.render('person', { person: person });
     }
+})
+
+router.get('/:name/delete', async (req, res) => {
+    var name = req.params.name;
+
+    await deleteBook(name);
+
+    res.redirect(303, '/book');
+
+});
+
+
+
+
+
+router.get('/', async (req, res) => {
+    const book = await readBook();
+
+    res.render('listing', { personlist: book })
+
 })
 
 
